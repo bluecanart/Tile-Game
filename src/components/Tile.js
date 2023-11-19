ef59c7b326613a020416b10774b93e60833b5c90
bluecanart <bluecanart@me.com>
2023-11-19 14:24:32 -0700

    Updated CSS for clue view

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -12,10 +12,14 @@ const TileContainer = styled.div`
   border-radius: 1vw;
   margin: 0.2vw;
   padding: 0.5vw 1vw;
-  background-color: ${props => props.color};
+  border-color: white;
+  border-style: solid;
+  border-width: ${props => (props.revealed ? '0.1vw' : '0')}; 
+  background-color: ${props => getColor(props.color, props.colorRevealOverride || props.revealed)};
   box-shadow: 0.3vw 0.3vw 0.8vw 0.01vw rgba(0, 0, 25, 0.25);
-  color: ${props => isDark(props.color) ? 'white' : 'black'};
+  color: ${props => isDark(getColor(props.color, props.colorRevealOverride || props.revealed)) ? 'white' : 'black'};
   cursor: pointer;
+  filter: ${props => (props.colorRevealOverride && props.revealed ? 'opacity(30%)' : 'none')}; 
   &:hover {
     filter: brightness(92%);
   }
@@ -26,6 +30,10 @@ const TextContainer = styled.p`
   font-weight: 400;
   margin: 0;
 `;
+
+const getColor = (color, isRevealed) => {
+  return isRevealed ? color : 'white';
+}
 
 const isDark = (color) => {
   switch(color){
@@ -43,7 +51,7 @@ const isDark = (color) => {
 
 function Tile({ color, text, onColorChange, colorRevealOverride, revealed, revealTile }) {
   return (
-    <TileContainer onClick={() => revealTile()} color={revealed || colorRevealOverride ? color : 'white'}>
+    <TileContainer color={color} colorRevealOverride={colorRevealOverride} revealed={revealed} onClick={() => revealTile()}>
       <TextContainer>{text}</TextContainer>
     </TileContainer>
   );
